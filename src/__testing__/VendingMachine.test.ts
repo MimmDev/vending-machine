adc5c5cf7b65963c73f38f54aea57ea8c6f870a7
Chris Mimm <mimmdev@gmail.com>
2021-01-20 20:49:25 +0000

    Initial user story tests for vending machine

---
+++ src/__testing__/VendingMachine.test.ts
@@ -0,0 +1,37 @@
+describe("Vending Machine", () => {
+  test.todo("should let the user add coins");
+
+  test.todo("should display INSERT COIN if no coins have been added");
+
+  test.todo("should display the current amount if coins have been added");
+
+  test.todo("should let the user view a list of available products");
+
+  test.todo(
+    "should allow a user to purchase a product if enough coins have been inserted"
+  );
+
+  test.todo(
+    "should reset the current amount to 0 after a purchase has been made"
+  );
+
+  test.todo(
+    "should display the chosen product's price if not enough coins have been inserted"
+  );
+
+  test.todo(
+    "should return change if a product cost less than the amount inserted"
+  );
+
+  test.todo(
+    "should allow the user to return all inserted coins from the machine"
+  );
+
+  test.todo(
+    "should display SOLD OUT if the user tries to purchase a sold out product"
+  );
+
+  test.todo(
+    "should display EXACT CHANGE ONLY if the machine does not have enough coins to make change"
+  );
+});
